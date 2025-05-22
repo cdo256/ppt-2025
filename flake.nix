d6f3a95d5f9c61a7f1d71a1609400dcdccb63775
Christina O'Donnell <cdo@mutix.org>
2025-05-22 18:26:20 +0100

    flake: Switch to flake-parts.

--- flake.nix
+++ flake.nix
@@ -3,57 +3,36 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     just-agda.url = "github:cdo256/just-agda";
-    #agda-finset.url = "github:input-output-hk/agda-finset";
   };
 
   outputs =
     inputs:
-    let
-      inherit (inputs) nixpkgs;
-      inherit (nixpkgs.lib.attrsets)
-        mapAttrs
-        mergeAttrsList
-        ;
-      inherit (nixpkgs.lib) genAttrs;
-      systems = [ "x86_64-linux" ];
-      # FIXME: This is clobbering other systems when more than one system is included.
-      # Use flake-utils?
-      forEachSystem =
-        fn:
-        mergeAttrsList (
-          map (
-            system:
-            let
-              res = fn nixpkgs.legacyPackages.${system};
-            in
-            mapAttrs (name: value: {
-              ${system} = value;
-            }) res
-          ) systems
-        );
-    in
-    forEachSystem (
-      pkgs:
-      let
-        agda = pkgs.agda.withPackages (ps: [
-          ps.standard-library
-          #ps.agda-cagetories
-        ]);
-        just-agda = inputs.just-agda.packages.${pkgs.system}.default;
-      in
-      {
-        packages = {
-          inherit agda just-agda;
-          default = just-agda;
-          inherit (pkgs) hello;
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } (top: {
+      systems = [
+        "x86_64-linux"
+      ];
+      perSystem =
+        { pkgs, ... }:
+        let
+          agda = pkgs.agda.withPackages (ps: [
+            ps.standard-library
+            #ps.agda-cagetories
+          ]);
+          just-agda = inputs.just-agda.packages.${pkgs.system}.default;
+        in
+        {
+          packages = {
+            inherit agda just-agda;
+            default = just-agda;
+          };
+          devShells.default = pkgs.mkShell {
+            buildInputs = [
+              agda
+              just-agda
+            ];
+          };
         };
-        devShells.default = pkgs.mkShell {
-          buildInputs = [
-            agda
-            just-agda
-          ];
-        };
-      }
-    );
+    });
 }
